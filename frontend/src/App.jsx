0ae4499772dfc433efbe05c02a847460cf3972e5
Kia Kajava <kia.kajava@aalto.fi>
2024-10-06 20:03:40 +0300

    added simple header bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { Typography, Box } from "@mui/material";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Box sx={{ padding: "2rem" }}>
-      <Typography variant="h1">Allergen tracker</Typography>
+      <Header/>
     </Box>
   );
 };
