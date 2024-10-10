c5a09e9de4a9e3d6fd5efe2d4c61d2a37fcd20ff
joonas <jompsku@icloud.com>
2024-10-10 12:57:28 +0300

    #17 implement basic google oauth functionality which allows the application to retrieve user name and email on a button click

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { CssBaseline } from "@mui/material";
 import LandingPage from "./routes/LandingPage";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./themes/theme";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./routes/LoginPage";
 import BasicLayout from "./layouts/BasicLayout";
@@ -19,11 +20,13 @@ const router = createBrowserRouter([
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <RouterProvider router={router} />
-      </CssBaseline>
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId="408468159516-9pf3e64af4knq24jm532g234ac3gqk3j.apps.googleusercontent.com">
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <RouterProvider router={router} />
+        </CssBaseline>
+      </ThemeProvider>
+    </GoogleOAuthProvider>
   );
 };
 
--- frontend/src/routes/LoginPage.jsx
+++ frontend/src/routes/LoginPage.jsx
@@ -1,7 +1,28 @@
 import { Box } from "@mui/material";
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const LoginPage = () => {
-  return <Box>Login</Box>;
+  const onSuccess = (response) => setToken(response.credential);
+  const onError = (error) => console.log(error);
+
+  const [token, setToken] = useState("");
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    if (token) {
+      const user = jwtDecode(token);
+      setProfile({ name: user.name, email: user.email });
+    }
+  }, [token]);
+  return (
+    <Box>
+      <GoogleLogin onSuccess={onSuccess} onError={onError} />
+      {JSON.stringify(profile)}
+    </Box>
+  );
 };
 
 export default LoginPage;
