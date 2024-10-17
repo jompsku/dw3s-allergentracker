d623fd09e0a6f4696196051c9a3919231cc35bf6
Kia Kajava <kia.kajava@aalto.fi>
2024-10-17 15:12:21 +0300

    added routing

---
+++ frontend/src/components/pages/UserPreferences.jsx
@@ -0,0 +1,11 @@
+import { Box, Button, Container } from "@mui/material"
+
+function UserPreferences() {
+  return (
+    <Container>
+      <Box>UserPreferences</Box>
+    </Container>
+  )
+}
+
+export default UserPreferences
