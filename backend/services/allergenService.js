a522fad34469af96060ef7e76e8c2a59e4a65deb
Milja Rantanen <milja.rantanen@gmail.com>
2024-11-10 22:09:42 +0200

    #22 change allergen detection logic

--- backend/services/allergenService.js
+++ backend/services/allergenService.js
@@ -1,23 +1,55 @@
-const Product = require("../database/models/Product")
+const Product = require("../database/models/Product");
+
+const THRESHOLD = 0.7;
+const MIN_AMOUNT = 3;
 
 const getProblematicProducts = async () => {
-  const problematicProductList = await Product.find({ isProblematic: true }).exec()
-  return problematicProductList
-}
+  const problematicProductList = await Product.find({
+    isProblematic: true,
+  }).exec();
+  return problematicProductList;
+};
+const getAllProducts = async () => {
+  const allProducts = await Product.find({}).exec();
+  return allProducts;
+};
+
+const getAllIngredients = (products) => {
+  return products.flatMap((product) => {
+    return product.ingredients;
+  });
+};
+
+const getCounts = (ingredients) => {
+  return ingredients.reduce((acc, ingredient) => {
+    acc[ingredient] = (acc[ingredient] || 0) + 1;
+  });
+};
 
 const getPossibleAllergens = async () => {
-  const problematicProducts = await getProblematicProducts()
-  const allIngredients = problematicProducts.flatMap((product) => {
-    return product.ingredients
-  })
-  const ingredientCounts = allIngredients.reduce((acc, ingredient) => {
-    acc[ingredient] = (acc[ingredient] || 0) + 1
-    return acc
-  }, {})
-  const commonIngredients = Object.entries(ingredientCounts)
-    .filter(([_, ingredientCount]) => ingredientCount > 1)
-    .map(([ingredient, _]) => ingredient)
-  return commonIngredients
-}
+  const problematicProducts = await getProblematicProducts();
+  const allProducts = await getAllProducts();
 
-module.exports = { getPossibleAllergens }
+  const allIngredientsInProblematicProducts =
+    getAllIngredients(problematicProducts);
+  const allIngredientsInAllProducts = getAllIngredients(allProducts);
+
+  const ingredientCountsInProblematicProducts = getCounts(
+    allIngredientsInProblematicProducts
+  );
+  const ingredientCountsInAllProducts = getCounts(allIngredientsInAllProducts);
+
+  const commonIngredients = Object.entries(
+    ingredientCountsInProblematicProducts
+  )
+    .filter(
+      ([ingredient, ingredientCount]) =>
+        ingredientCount / ingredientCountsInAllProducts[ingredient] >
+          THRESHOLD && ingredientCountsInAllProducts[ingredient] > MIN_AMOUNT
+    )
+    .map(([ingredient, ingredientCount]) => ingredient);
+
+  return commonIngredients;
+};
+
+module.exports = { getPossibleAllergens };
