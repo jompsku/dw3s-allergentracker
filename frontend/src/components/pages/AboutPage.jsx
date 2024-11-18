2aa919ea26ab7850720fc242896e92345db25569
Milja Rantanen <milja.rantanen@gmail.com>
2024-11-18 11:57:21 +0200

    add about page contents

--- frontend/src/components/pages/AboutPage.jsx
+++ frontend/src/components/pages/AboutPage.jsx
@@ -1,5 +1,24 @@
+import { Box, Typography } from "@mui/material";
+
 const AboutPage = () => {
-  return (<p>about</p>);
+  return (
+    <Box>
+      <Typography variant="h2" sx={{ marginTop: '25px' }}>About Allergen Tracker</Typography>
+      <p>
+        Allergen Tracker is a project developed as part of the course Design of WWW Sevices (CS-E4400) at 
+        Aalto University.<br/>
+        Its purpose is to help users track allergens in cosmetic products and identify 
+        ingredients that may cause allergic reactions.
+      </p>
+      <Typography variant="h3">Contact Information</Typography>
+      <p>For inquiries, please feel free to reach out to any of the following contacts:</p>
+      <ul>
+        <li><a href="mailto:milja.rantanen@aalto.fi" style={{ color: 'black' }}>milja.rantanen@aalto.fi</a></li>
+        <li><a href="mailto:kia.kajava@aalto.fi" style={{ color: 'black' }}>kia.kajava@aalto.fi</a></li>
+        <li><a href="mailto:joonas.e.sorvari@aalto.fi" style={{ color: 'black' }}>joonas.e.sorvari@aalto.fi</a></li>
+      </ul>
+    </Box>
+  );
 };
 
 export default AboutPage;
