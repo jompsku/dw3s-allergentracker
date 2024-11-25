5954c15b9b1f7bd9169e49faf454c2b9fb628b68
Kia Kajava <kia.kajava1@gmail.com>
2024-11-25 17:20:52 +0200

    fixed routes and changed backend from redirecting to login to refetching auth each time

--- frontend/src/components/pages/AppRoutes.jsx
+++ frontend/src/components/pages/AppRoutes.jsx
@@ -1,29 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import GeneralInfoPage from "./GeneralInfoPage";
-import LoginPage from "./LoginPage";
 import LandingPage from "./LandingPage";
-import PrivateRoute from "../PrivateRoute";
 import { Container, Typography } from "@mui/material";
 import AboutPage from "./AboutPage";
 import TosPage from "./TosPage";
 import PrivacyPolicyPage from "./PrivacyPolicyPage";
 import ProtectedRoutes from "../../utils/ProtectedRoutes";
-import { useAuth } from "../../hooks/useAuth";
+import LoginPage from "./LoginPage";
 
 const AppRoutes = () => {
-  
-  const getUser = async () => {
-    return await useAuth();
-  };
-  const { user } = getUser();
-
   return (
     <Routes>
-      {user ? (
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/how-it-works" element={<GeneralInfoPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/terms-of-service" element={<TosPage />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+      <Route element={<ProtectedRoutes />}>
         <Route path="/" element={<LandingPage />} />
-      ) : (
-        <Route path="/login" element={<LoginPage />} />
-      )}
+      </Route>
       <Route
         path="*"
         element={
@@ -32,13 +27,6 @@ const AppRoutes = () => {
           </Container>
         }
       />
-      <Route element={<ProtectedRoutes />}>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/how-it-works" element={<GeneralInfoPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/terms-of-service" element={<TosPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-      </Route>
     </Routes>
   );
 };
