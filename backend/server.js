5a18c4019818c154b4ce422cb71616585699cc5d
Milja Rantanen <milja.rantanen@gmail.com>
2024-11-25 23:07:01 +0200

    change frontend origin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: process.env.NODE_ENV === "production" ? "https://allergentracker.onrender.com" : "http://localhost:5173",
+  origin: process.env.NODE_ENV === "production" ? "https://dw3s-allergentracker.onrender.com" : "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
