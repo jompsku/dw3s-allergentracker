9705e1668d726b71f0d6dd882f1b087bd9dc8466
Kia Kajava <kia.kajava1@gmail.com>
2024-11-25 15:57:06 +0200

    -

--- frontend/src/components/pages/AppRoutes.jsx
+++ frontend/src/components/pages/AppRoutes.jsx
@@ -1,30 +1,29 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import GeneralInfoPage from "./GeneralInfoPage";
 import LoginPage from "./LoginPage";
 import LandingPage from "./LandingPage";
 import PrivateRoute from "../PrivateRoute";
-import { useAuth } from "../../hooks/useAuth";
 import { Container, Typography } from "@mui/material";
 import AboutPage from "./AboutPage";
 import TosPage from "./TosPage";
 import PrivacyPolicyPage from "./PrivacyPolicyPage";
+import ProtectedRoutes from "../../utils/ProtectedRoutes";
+import { useAuth } from "../../hooks/useAuth";
 
 const AppRoutes = () => {
-  const { user } = useAuth();
+  
+  const getUser = async () => {
+    return await useAuth();
+  };
+  const { user } = getUser();
 
   return (
     <Routes>
-      <Route path="/" element={<PrivateRoute element={<LandingPage />} />} />
-
-      <Route path="/how-it-works" element={<GeneralInfoPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/terms-of-service" element={<TosPage />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-      <Route
-        path="/login"
-        element={user ? <Navigate to="/" replace /> : <LoginPage />}
-      />
-
+      {user ? (
+        <Route path="/" element={<LandingPage />} />
+      ) : (
+        <Route path="/login" element={<LoginPage />} />
+      )}
       <Route
         path="*"
         element={
@@ -33,6 +32,13 @@ const AppRoutes = () => {
           </Container>
         }
       />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/how-it-works" element={<GeneralInfoPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/terms-of-service" element={<TosPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+      </Route>
     </Routes>
   );
 };
