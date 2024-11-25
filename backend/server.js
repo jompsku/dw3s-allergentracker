9c06864cdb0bfe866001f654d4d4ead5c0fa13a9
Milja Rantanen <milja.rantanen@gmail.com>
2024-11-25 20:54:36 +0200

    Update cors configuration to use the deployed frontend domain

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.NODE_ENV === "production" ? "https://allergentracker.onrender.com" : "http://localhost:5173",
+  credentials: true,
   credentials: true,
 };
 app.use(cors(corsOptions));
