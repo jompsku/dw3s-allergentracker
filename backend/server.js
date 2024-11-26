b2626f9d61dc2e2006914fb6cef9c60183cf0efa
joonas <jompsku@icloud.com>
2024-11-26 22:49:16 +0200

    set sameSite to none

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24,
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "None",
     },
   })
 );
