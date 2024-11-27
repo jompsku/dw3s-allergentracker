c154171359210ce76c00f5eceeefaacd12a88fa1
Kia Kajava <kia.kajava1@gmail.com>
2024-11-27 19:04:52 +0200

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { AppProvider } from "@toolpad/core/AppProvider";
 import theme from "./themes/theme";
 
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
 
 const queryClient = new QueryClient();
 
@@ -13,9 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppProvider theme={theme}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </AppProvider>
     </QueryClientProvider>
   </StrictMode>
