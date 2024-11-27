47b2b01ad786fc176aa647a88dbc58f960073683
Kia Kajava <kia.kajava1@gmail.com>
2024-11-27 18:55:13 +0200

    fixed navigate to login when logging out

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,21 +1,23 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
   useEffect(() => {
     axios
-    .get("http://localhost:8080/auth/current_user", {
-      withCredentials: true,
-    })
-    .then((response) => {
-      setUser(response.data);
-    })
-    .catch(() => setUser(null))
-    .finally(() => setLoading(false));
+      .get("http://localhost:8080/auth/current_user", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch(() => setUser(null))
+      .finally(() => setLoading(false));
   }, []);
 
   const logout = async () => {
@@ -24,6 +26,7 @@ export const AuthProvider = ({ children }) => {
         withCredentials: true,
       });
       setUser(null);
+      navigate("/login");
     } catch (error) {
       console.error("Error logging out:", error);
     }
